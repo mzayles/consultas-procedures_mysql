6a9f783191919e22f010ed475f762457ebb4f785
Mzayles <marisouza0230@gmail.com>
2023-09-09 17:15:59 -0300

    Ex. 04

--- lista_exercicios.sql
+++ lista_exercicios.sql
@@ -11,3 +11,9 @@ SELECT l.titulo, a.nome
 FROM livros l
 INNER JOIN autores a ON l.autor_id = a.id
 WHERE nome = 'J.K. Rowling';
+
+-- Ex. 04
+SELECT a.nome, m.curso
+FROM alunos a
+INNER JOIN matriculas m ON a.id = m.aluno_id
+WHERE curso = 'Engenharia de Software';

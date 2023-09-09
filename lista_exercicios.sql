204cce56741b7b2c9c5e235d8f3673f07d39cb32
Mzayles <marisouza0230@gmail.com>
2023-09-09 17:35:27 -0300

    Ex. 19

--- lista_exercicios.sql
+++ lista_exercicios.sql
@@ -95,3 +95,9 @@ FROM vendas
 GROUP BY produto
 ORDER BY total_vendas
 LIMIT 1;
+
+-- Ex. 19
+SELECT a.nome, COUNT(aluno_id) AS total_matriculas
+FROM alunos a
+INNER JOIN matriculas m ON a.id = m.aluno_id
+GROUP BY a.nome;

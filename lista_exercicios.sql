cc6a0814e1ed0903cb36f0f1f589ae03f0475bf3
Mzayles <marisouza0230@gmail.com>
2023-09-09 17:15:25 -0300

    Ex. 03

--- lista_exercicios.sql
+++ lista_exercicios.sql
@@ -5,3 +5,9 @@ SELECT titulo FROM livros;
 SELECT nome
 FROM autores 
 WHERE nascimento < '1900-01-01';
+
+-- Ex. 03
+SELECT l.titulo, a.nome
+FROM livros l
+INNER JOIN autores a ON l.autor_id = a.id
+WHERE nome = 'J.K. Rowling';
